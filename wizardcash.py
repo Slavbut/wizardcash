f430b78f514d2d6192306a7f4fbad5ea972b90ab
Slava <164445381+Slavbut@users.noreply.github.com>
2025-03-07 11:36:05 +0000

    Add files via upload

---
+++ wizardcash.py
@@ -0,0 +1,8 @@
+def wizardcash():
+  wizardcashh = input("wizardcash? ")
+  if wizardcashh == "wizardcash":
+    print("wizardcash!")
+  else:
+    print("wizardcash...")
+    wizardcash()
+wizardcash()
